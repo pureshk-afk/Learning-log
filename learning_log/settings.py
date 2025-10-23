3bf3a51308a12718ffc970d5a8d7bd2c07576de4
Ksenia Gurenko <gurenkoksenia0@gmail.com>
2025-10-23 19:24:06 +0300

    add host setting

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-*dgl@p9_s2p@5lfrl@*ad=5qvcjslw4jut-bcs)w$)dohp-&bv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("LL_HOST", "localhost")
+]
 
 
 # Application definition
